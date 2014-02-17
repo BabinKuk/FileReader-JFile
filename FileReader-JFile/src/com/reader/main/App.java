222d8031a15998d13698006e11e3ec46e8242aab
BabinKuk <nikola.babic@gmail.com>
2014-02-17 10:57:12 +0100

    format

--- FileReader-JFile/src/com/reader/main/App.java
+++ FileReader-JFile/src/com/reader/main/App.java
@@ -4,18 +4,18 @@ import com.reader.filehandler.FileHandler;
 
 /**
  * reading files with FileReader
+ * 
  * @author nbabic
- *
+ * 
  */
 public class App {
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		FileHandler fileHandler = new FileHandler();
 		fileHandler.handleInputs();
-
 	}
 
 }
